a47179b5b2dd2880d44a7e5de5aa5738e8f9266a
LorenzoNicco <lorenzoniccolai91@gmail.com>
2022-12-21 14:50:12 +0100

    Add immagine nella cartella e steps come commenti

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*
+    1. INIZIO
+    2. Creare il div originale
+    3. Stilizzarlo in css e dare le regole generali
+    4. usare il ciclo per creare tante iterazioni quante ne abbiamo bisogno
+    5. usare if per modificare i multipli di 3
+    6. Usare if per modificare i multipli di 5
+    7. Usare if per modificare i numeri di 3 e 5 insieme
+*/
