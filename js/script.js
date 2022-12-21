5c85fbd37dbe03203a7baa0c281e03ed17da842a
LorenzoNicco <lorenzoniccolai91@gmail.com>
2022-12-21 14:57:07 +0100

    Update steps in js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 /*
     1. INIZIO
-    2. Creare il div originale
-    3. Stilizzarlo in css e dare le regole generali
-    4. usare il ciclo per creare tante iterazioni quante ne abbiamo bisogno
-    5. usare if per modificare i multipli di 3
-    6. Usare if per modificare i multipli di 5
-    7. Usare if per modificare i numeri di 3 e 5 insieme
+    2. usare il ciclo per creare tante iterazioni quante ne abbiamo bisogno
+    3. usare if per modificare i multipli di 3
+    4. Usare if per modificare i multipli di 5
+    5. Usare if per modificare i numeri di 3 e 5 insieme
 */
+
